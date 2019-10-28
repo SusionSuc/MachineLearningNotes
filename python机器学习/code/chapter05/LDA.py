40347385cdb6174576fab94bf82766dfd4229fc7
susion <susionsuc@163.com>
2019-10-28 10:07:58 +0800

    update lda

--- python机器学习/code/chapter05/LDA.py
+++ python机器学习/code/chapter05/LDA.py
@@ -2,5 +2,130 @@
 # LDA 假设数据呈正态分布,并假设类具有相同的协方差矩阵，并且这些特征在统计上彼此独立。
 # 它的主要步骤如下 :
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
+df_wine = pd.read_csv('./wine.data', header=None)
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'Proline']
+
+# Splitting the data into 70% training and 30% test subsets.
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=0)
+
+# LDA 实现步骤
+
+# 1. 标准化
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.fit_transform(X_test)
+
+# 2.计算每个类的d维均值向量
+
+np.set_printoptions(precision=4)
+
+mean_vecs = []
+
+# print(X_train_std[y_train == 1])
+# print(np.mean(X_train_std[y_train == 1], axis = 0))
+
+# 每个均值向量 m_i 存储对应分类样本i的特征平均值u_m
+for label in range(1, 4):  # range -> [)
+    # print('label', label)
+    mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label - 1]))
+
+# 3. 计算类内散布矩阵
+
+d = 13  # number of features
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.zeros((d, d))  # scatter matrix for each class
+    for row in X_train_std[y_train == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)  # make column vectors
+        class_scatter += (row - mv).dot((row - mv).T)
+    S_W += class_scatter  # sum class scatter matrices
+
+print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+
+print('Class label distribution: %s' % np.bincount(y_train)[1:])
+
+d = 13  # number of features
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T)
+    S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' % (S_W.shape[0],
+                                                     S_W.shape[1]))
+
+# 4. 计算类见散布矩阵
+# Compute the between-class scatter matrix:
+
+mean_overall = np.mean(X_train_std, axis=0)
+d = 13  # number of features
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train[y_train == i + 1, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1)  # make column vector
+    mean_overall = mean_overall.reshape(d, 1)  # make column vector
+    S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+
+print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+
+# ## Selecting linear discriminants for the new feature subspace
+
+# Solve the generalized eigenvalue problem for the matrix $S_W^{-1}S_B$:
+
+
+# 5. 分解为特征值和特征向量
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+
+# Sort eigenvectors in descending order of the eigenvalues:
+
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+               for i in range(len(eigen_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+
+# Visually confirm that the list is correctly sorted by decreasing eigenvalues
+
+print('Eigenvalues in descending order:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+
+# 降序排列 特征值和特征向量后，后面的步骤就和PCA差不多了。
+# 6. 创建转换矩阵
+# 7. 通过转换矩阵转换训练集，然后用转换后的样本数据来训练模型
+
+# scikit learn 中 LDA
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from base.plotClassifier import plot_decision_regions
+
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+# plt.savefig('images/05_09.png', dpi=300)
+plt.show()
 
