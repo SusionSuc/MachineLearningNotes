31f679c6cca15025453c95c14643c0933af5c737
susion <susionsuc@163.com>
2019-10-31 10:04:03 +0800

    update pipeline

---
+++ python机器学习/code/chapter06/pipeline.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.pipeline import make_pipeline
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv("wdbc.data", header=None)
+
+print(df.head())
+print(df.shape)
+
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)  # 将 'M' or 'B' 编码为整数
+
+print(le.classes_)
+print(le.transform(['M', "B"]))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.20,
+                                                    stratify=y,
+                                                    random_state=1)
+
+pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2), LogisticRegression(random_state=1))
+pipe_lr.fit(X_train, y_train)
+# y_pred = pipe_lr.predict(X_test)
+print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+
+# ## K-fold cross-validation
+
+from sklearn.model_selection import StratifiedKFold
+
+kfold = StratifiedKFold(n_splits=10, random_state=1).split(X_train, y_train)
+# print(kfold)
+scores = []
+for k, (train, test) in enumerate(kfold):
+    pipe_lr.fit(X_train[train], y_train[train])
+    score = pipe_lr.score(X_train[test], y_train[test])
+    scores.append(score)
+    # print('Fold: %2d, Class dist.: %s, Acc: %.3f' % (k + 1, np.bincount(y_train[train]), score))
+
+print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
